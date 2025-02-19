c59bf12462b2e4a39b02f15300dd2d92caa7861a
Chavela <angie87wong@gmail.com>
2025-02-19 18:24:42 -0300

    fix: elimino chackraui

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -15,4 +16,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { db, storage };
+export { db, storage, app };
