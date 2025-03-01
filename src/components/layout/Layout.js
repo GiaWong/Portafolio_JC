1ef0dfab6005b79ba9f044b219a2ec750cdd9b79
Chavela <angie87wong@gmail.com>
2025-03-01 20:50:30 -0300

    fix: corrijo implementacion del Layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,54 +1,4 @@
-import { useState } from "react";
-import { Link } from "react-scroll";
-
-const links = [
-  { label: "Inicio", to: "inicioSection" },
-  { label: "Quién soy", to: "aboutSection" },
-  { label: "Experiencia", to: "experience" },
-  { label: "Proyectos", to: "projectsSection" },
-  { label: "Contacto", to: "contacto" },
-];
-
-const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => setMenuOpen(!menuOpen);
-  const closeMenu = () => setMenuOpen(false);
-
-  return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <h2 className="logo">Mi Portafolio</h2>
-
-        {/* Botón menú hamburguesa (solo en móviles) */}
-        <button 
-          className="navbar-toggle md:hidden" 
-          onClick={toggleMenu} 
-          aria-label="Toggle menu"
-        >
-          ☰
-        </button>
-
-        {/* Lista de enlaces con scroll suave */}
-        <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
-          {links.map(({ label, to }, index) => (
-            <li key={index}>
-              <Link 
-                to={to} 
-                smooth={true} 
-                duration={500} 
-                offset={-70} // Ajuste para la navbar fija
-                onClick={closeMenu}
-              >
-                {label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </nav>
-  );
-};
+import Navbar from "@/components/layout/Navbar"; 
 
 const Footer = () => (
   <footer className="footer">
@@ -59,7 +9,7 @@ const Footer = () => (
 export default function Layout({ children }) {
   return (
     <div>
-      <Navbar />
+      <Navbar /> 
       <main className="container">{children}</main>
       <Footer />
     </div>
